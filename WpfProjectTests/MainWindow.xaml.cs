a4066475657be71d24ac30ad06937bf57e1cceb6
Thibault Poels <thibault.poels@immalle.be>
2017-10-02 10:06:03 +0200

    Change1

--- WpfProjectTests/MainWindow.xaml.cs
+++ WpfProjectTests/MainWindow.xaml.cs
@@ -24,5 +24,46 @@ namespace WpfProjectTests
         {
             InitializeComponent();
         }
+
+        private void knop2_MouseEnter(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("Hallo wereld!", "Hallo", MessageBoxButton.OKCancel, MessageBoxImage.Warning);
+        }
+
+        private void knop1_Click(object sender, RoutedEventArgs e)
+        {
+            double top = 0;
+            double left = 0;
+
+            top = mijnRechthoek.Margin.Top;
+            left = mijnRechthoek.Margin.Left;
+
+            left++;
+
+            mijnRechthoek.Stroke = Brushes.BlueViolet;
+            mijnRechthoek.Margin = new Thickness(left, top, 0, 0);
+        }
+
+        private void knop3_Click(object sender, RoutedEventArgs e)
+        {
+            Line Lijn = new Line();
+            Lijn.X1 = 35;
+            Lijn.Y1 = 4;
+            Lijn.X2 = 76;
+            Lijn.Y2 = 190;
+            Lijn.Stroke = new SolidColorBrush(Colors.DarkSalmon);
+            mijnCanvas.Children.Add(Lijn);
+        }
+
+        private void knop4_Click(object sender, RoutedEventArgs e)
+        {
+            Ellipse ellipse1 = new Ellipse();
+            ellipse1.Width = 50;
+            ellipse1.Height = 50;
+            ellipse1.Margin = new Thickness(10, 10, 0, 0);
+            ellipse1.Stroke = new SolidColorBrush(Colors.PaleVioletRed);
+            ellipse1.Fill = Brushes.Purple;
+            mijnCanvas.Children.Add(ellipse1);
+        }
     }
 }
